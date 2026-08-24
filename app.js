8828aa6a2660677920345cdf3d836d7bcc0fb24a
awrcdirector-hub <awrcdirector@gmail.com>
2026-08-24 23:22:35 +1200

    Add files via upload

--- app.js
+++ app.js
@@ -1947,8 +1947,8 @@ function sendNotification(title, body, options = {}) {
     navigator.serviceWorker.ready
       .then((registration) => registration.showNotification(title, {
         body,
-        icon: "/logbook-icon.png",
-        badge: "/logbook-icon.png",
+        icon: "/logbook-icon-v2.png",
+        badge: "/logbook-icon-v2.png",
         tag: options.tag || title,
         requireInteraction: Boolean(options.requireInteraction)
       }))
@@ -1961,7 +1961,7 @@ function sendNotification(title, body, options = {}) {
 function showWindowNotification(title, body, options = {}) {
   new Notification(title, {
     body,
-    icon: "/logbook-icon.png",
+    icon: "/logbook-icon-v2.png",
     tag: options.tag || title,
     requireInteraction: Boolean(options.requireInteraction)
   });
